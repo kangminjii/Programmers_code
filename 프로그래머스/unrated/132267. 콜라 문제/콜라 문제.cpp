e0c6f8a6a1c54c68b6816e0d047fb4a78547f70e
Kang-MinJi <87412863+kangminjii@users.noreply.github.com>
2023-05-03 14:52:00 +0900

    [unrated] Title: 콜라 문제, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int n) {
+    int answer = 0;
+    
+    int filled, repeatBottle, others = 0;
+    
+    filled = n;
+    answer += (n / a) * b;
+    
+    filled = (n / a) * b;
+    others = n % a;
+   
+    while(repeatBottle >= a)
+    {
+        repeatBottle = filled + others;
+        answer += (repeatBottle / a) * b;
+        
+        filled = (repeatBottle / a) * b;
+        others = repeatBottle % a;        
+    }
+    
+    return answer;
+}
