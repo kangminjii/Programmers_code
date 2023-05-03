b271becd19cd1c5e929ce80283384016325d12fa
Kang-MinJi <87412863+kangminjii@users.noreply.github.com>
2023-05-03 17:06:37 +0900

    [unrated] Title: 콜라 문제, Time: 0.01 ms, Memory: 3.7 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/README.md
+++ 프로그래머스/unrated/132267. 콜라 문제/README.md
@@ -1,10 +1,10 @@
 # [unrated] 콜라 문제 - 132267 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132267) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132267#) 
 
 ### 성능 요약
 
-메모리: 4.02 MB, 시간: 0.01 ms
+메모리: 3.7 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.cpp
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.cpp
@@ -4,14 +4,14 @@
 using namespace std;
 
 int solution(int a, int b, int n) {
+   
     int answer = 0;
-    
-    int filled, repeatBottle, others = 0;
+    int repeatBottle, filled, others;
     
     answer += (n / a) * b;
-    
     filled = (n / a) * b;
     others = n % a;
+   
    
     while(repeatBottle >= a)
     {
@@ -19,8 +19,21 @@ int solution(int a, int b, int n) {
         answer += (repeatBottle / a) * b;
         
         filled = (repeatBottle / a) * b;
-        others = repeatBottle % a;        
+        others = repeatBottle % a;
     }
+    
+    
+    /*
+    // 다른 풀이(변수 간소화)
+    int answer = 0;
+   
+    while(n >= a)
+    {
+        int temp = (n/a)*b;
+        answer += temp;
+        n = (n/a)*b + n%a;
+     }
+    */ 
     
     return answer;
 }
